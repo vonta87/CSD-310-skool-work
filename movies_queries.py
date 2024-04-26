23d615655f9f6d00dd8eba71d25d54c7d12986a3
vonta87 <156873891+vonta87@users.noreply.github.com>
2024-04-25 17:05:16 -0700

    Add files via upload

---
+++ movies_queries.py
@@ -0,0 +1,42 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "movies_user",
+    "password": "876783Zahir#",
+    "host": "127.0.0.1",
+    "database": "movies",
+    "raise_on_warnings": True
+
+}
+
+try: 
+    db =mysql.connector.connect(**config)
+
+    print("\n Database user {} connected to MySQL on {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+    cursor = db.cursor()
+    cursor.execute("SELECT studio_id,studio_name,genre_id,genre_name FROM studio,genre")
+    studio = cursor.fetchall()
+    genre = cursor.fetchall()
+    film = cursor.fetchall(HAVING COUNT(*) <= 2)
+    for director in film:
+        print("First Name: {}\n Last Name: {}\n".format(director[0],director[1],director[2])) -- three 
+        fields
+
+    db.close()
+
+
